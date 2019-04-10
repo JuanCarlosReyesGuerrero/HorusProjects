39d1e951a7e4a9ac9a105a8a13eb44cf20c7985e
juanreyes <juan.reyes@cjtytsoftware.com>
2019-04-10 15:13:03 -0500

    Proyecto inicial
    Proyecto inicial

---
+++ Horus.Dao/DataProvider.cs
@@ -0,0 +1,35 @@
+﻿namespace Horus.Dao
+{
+    public enum DataProvider
+    {
+        /// <summary>
+        /// The oracle
+        /// </summary>
+        Oracle,
+
+        /// <summary>
+        /// The SQL server
+        /// </summary>
+        SqlServer,
+
+        /// <summary>
+        /// The OLE database
+        /// </summary>
+        OleDb,
+
+        /// <summary>
+        /// The ODBC
+        /// </summary>
+        Odbc,
+
+        /// <summary>
+        /// The mysql
+        /// </summary>
+        Mysql,
+
+        /// <summary>
+        /// The postgres SQL
+        /// </summary>
+        PostgreSql
+    }
+}
---
+++ Horus.Dao/IDBManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+
+namespace Horus.Dao
+{
+    public interface IDBManager
+    {
+        DataProvider ProviderType
+        {
+            get;
+            set;
+        }
+
+        string ConnectionString
+        {
+            get;
+            set;
+        }
+
+        IDbConnection Connection
+        {
+            get;
+        }
+
+        IDbTransaction Transaction
+        {
+            get;
+        }
+
+        IDataReader DataReader
+        {
+            get;
+        }
+        IDbCommand Command
+        {
+            get;
+        }
+
+        IDbDataParameter[] Parameters
+        {
+            get;
+        }
+
+        void Open();
+        void BeginTransaction();
+        void CommitTransaction();
+        void CreateParameters(int paramsCount);
+        void AddParameters(int index, string paramName, object objValue);
+        IDataReader ExecuteReader(string commandText, CommandType commandType);
+        DataSet ExecuteDataSet(string commandText, CommandType commandType);
+        object ExecuteScalar(string commandText, CommandType commandType);
+        int ExecuteNonQuery(string commandText, CommandType commandType);
+        void CloseReader();
+        void Close();
+        void Dispose();
+    }
+}
